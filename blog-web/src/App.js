0ebab82083e70ff0f951697938a8880adadad345
Mayur Kode <mayurkode7@gmail.com>
2022-06-01 00:07:37 +0530

    ROUTING: LINK PAGES WITH ROUTES

--- blog-web/src/App.js
+++ blog-web/src/App.js
@@ -1,11 +1,22 @@
 
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
+
+// pages 
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import CreateBlog from './pages/CreateBlog'
 function App() {
   return (
-    <div className="App">
-      <h1>Blog</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/sign-in' element={<SignIn />} ></Route>
+        <Route path='/about' element={<About />} ></Route>
+        <Route path='/create-blog' element={<CreateBlog />} ></Route>
+      </Routes>
+    </Router>
   );
 }
 
