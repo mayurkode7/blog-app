a9997d2dce62ee4486c1f26b1e7fd09c92f7b60a
Mayur Kode <mayurkode7@gmail.com>
2022-06-01 00:16:43 +0530

    ROUTING: NAVBAR WITH CSS

--- blog-web/src/App.js
+++ blog-web/src/App.js
@@ -1,22 +1,28 @@
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-
+import './App.css'
 
 // pages 
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import About from './pages/About'
 import CreateBlog from './pages/CreateBlog'
+
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/create-blog">Create Blog</Link>
+      </nav>
       <Routes>
         <Route path='/' element={<Home />} ></Route>
         <Route path='/sign-in' element={<SignIn />} ></Route>
         <Route path='/about' element={<About />} ></Route>
         <Route path='/create-blog' element={<CreateBlog />} ></Route>
       </Routes>
-    </Router>
+    </Router >
   );
 }
 
