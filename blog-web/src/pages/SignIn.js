55ddfc8eaecbbef08126854fa3489f758f09f63f
Mayur Kode <mayurkode7@gmail.com>
2022-06-01 00:43:52 +0530

    AUTHENTICATION : add basic sign in with pop up function and add details to local storage

--- blog-web/src/pages/SignIn.js
+++ blog-web/src/pages/SignIn.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { auth, provider } from '../firebase-config'
+import { signInWithPopup } from 'firebase/auth'
 
 function SignIn() {
+
+
+  const signInWithGoogle = async () => {
+    let signIn = await signInWithPopup(auth, provider)
+    localStorage.setItem("signIn", signIn?.user?.email)
+  }
+
   return (
     <div className='loginPage'>
       <p> Sign In with Google to Continue..</p>
-      <button className='login-with-google-btn'> Sign In</button>
+      <button className='login-with-google-btn' onClick={() => signInWithGoogle()}> Sign In</button>
     </div>
   )
 }
