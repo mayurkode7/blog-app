0e1c3f2fb234ec5e87690706fe7b042de4387a77
Mayur Kode <mayurkode7@gmail.com>
2022-06-03 01:43:45 +0530

    CREATE BLOG: PAGE LAYOUT AND FORM

--- blog-web/src/pages/CreateBlog.js
+++ blog-web/src/pages/CreateBlog.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function CreateBlog() {
+
+  const [title, setTitle] = useState("")
+  const [blogText, setBlogText] = useState("")
+
+  const createBlog = () => { }
+
   return (
-    <div>Create Blog</div>
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Blog</h1>
+        <div className="inputGp">
+          <label> Title:</label>
+          <input
+            placeholder="Title..."
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </div>
+        <div className="inputGp">
+          <label> Blog:</label>
+          <textarea
+            placeholder="Blog..."
+            onChange={(event) => {
+              setBlogText(event.target.value);
+            }}
+          />
+        </div>
+        <button onClick={createBlog}> Submit Blog</button>
+      </div>
+    </div>
   )
 }
 
