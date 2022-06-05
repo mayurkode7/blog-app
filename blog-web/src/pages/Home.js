e5f7b9b955d38ee58d9afcb15c2f2f2f2d757392
Mayur Kode <mayurkode7@gmail.com>
2022-06-06 02:23:31 +0530

    FETCH DATA FROM FIREBASE COLLECTION : HOME SCREEN BASIC

--- blog-web/src/pages/Home.js
+++ blog-web/src/pages/Home.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../firebase-config'
 
 function Home() {
+
+  const [blogList, setBlogList] = useState(null)
+
+  const blogCollectionRef = collection(db, "blogs")
+
+  useEffect(() => {
+
+    const getBlogList = async () => {
+
+      const data = await getDocs(blogCollectionRef)
+      setBlogList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+
+    getBlogList()
+
+  }, [])
+
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+
+      {blogList && blogList.map(item => <p> {item.title}</p>)}
+    </>
   )
 }
 
