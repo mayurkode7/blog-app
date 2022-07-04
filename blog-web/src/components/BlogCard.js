7ad29e47bf5e9140840cd9098989d5f8e5e28475
Mayur Kode <mayurkode7@gmail.com>
2022-07-05 01:38:16 +0530

    SHOW DELETE ONLY WHEN USER IS AUTHENTICATED

--- blog-web/src/components/BlogCard.js
+++ blog-web/src/components/BlogCard.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { auth } from '../firebase-config'
 
-function BlogCard({ post }) {
+function BlogCard({ post, deleteBlog, isAuth }) {
+
   return (
     <div className="post">
       <div className="postHeader">
         <div className="title">
           <h1> {post.title}</h1>
         </div>
+        {(auth.currentUser.email == post.author.email) &&
+          <div className='deletePost'>
+            <button onClick={() => deleteBlog(post.id)}> &#126465;
+            </button>
+          </div>}
+
       </div>
       <div className="postTextContainer"> {post.blogText} </div>
       <h3>@ {post.author?.name}</h3>
